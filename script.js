3b8b7d7ca4736bbce32157cb5c60f58d152918db
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-26 11:39:11 -0400

    add spinner before showing the countdown

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 
 const currentYear = new Date().getFullYear();
@@ -23,10 +24,18 @@ function updateCountdown() {
   const m = Math.floor(diff / (1000 * 60)) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
+  // Add values to DOM
   days.innerText = d;
   hours.innerText = h < 10 ? '0' + h : h;
   minutes.innerText = m < 10 ? '0' + m : m;
   seconds.innerText = s < 10 ? '0' + s : s;
 }
 
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
 setInterval(updateCountdown, 1000);
